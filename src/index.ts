4b2a191faa7a6f6206418efb819203d97c5da94e
Bryan Emerson <bryan.l.emerson@gmail.com>
2023-04-04 11:09:43 -0700

    arrays

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-let sales: number = 123_456_789;
-let course: string = "TypeScript";
-let isPublished: boolean = true;
-let level;
-
-function render(document) {
-  console.log(document)
-}
+let numbers = [1, 2, '3']
